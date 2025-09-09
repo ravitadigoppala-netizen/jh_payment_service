598169a40010e56cfbebcd72734a4a229e0841dd
Jyoti Arya <Jyoti.Arya@celsiortech.com>
2025-09-09 17:31:28 +0530

    payment_refund

--- jh_payment_service/Program.cs
+++ jh_payment_service/Program.cs
@@ -1,7 +1,9 @@
+using PaymentAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<RefundService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
