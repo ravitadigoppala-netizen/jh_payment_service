935edf7543b945ae42763f534f3269f2789d54da
Istiyak-Celsior <md.istayaque@celsiortech.com>
2025-09-09 15:02:51 +0530

    added structure

--- jh_payment_service/Controllers/WeatherForecastController.cs
+++ jh_payment_service/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using jh_payment_service.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace jh_payment_service.Controllers
@@ -28,6 +29,12 @@ namespace jh_payment_service.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("healthCheck")]
+        public ResponseModel healthCheck()
+        {
+            return ResponseModel.Ok("Working");
         }
     }
 }
