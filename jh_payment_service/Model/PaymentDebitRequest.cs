0ae7eafbb6eb0a9355a5960d80e252a1e4ce8b2b
HarishKumar-Celsior <Harish.Kumar@celsiortech.com>
2025-09-12 17:30:44 +0530

    Added product Id to debit payment request and removed sender and receiver for both credit and debit payment request

---
+++ jh_payment_service/Model/PaymentDebitRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace jh_payment_service.Model
+{
+    public class PaymentDebitRequest : PaymentRequest
+    {
+        /// <summary>
+        /// Represents the unique identifier of the product.
+        /// </summary>
+        public string ProductId { get; set; }
+    }
+}
